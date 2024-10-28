d820c00723b7941b4054f3535b7146aadbed7178
Lucas Pinheiro <ludafepi123456@gmail.com>
2024-10-28 15:13:26 -0400

    Minify

--- javascript/simpleConfirm.js
+++ javascript/simpleConfirm.js
@@ -1,27 +1,6 @@
+function confirmar(title, description) {
 
-
-
-
-function confirmar(title, description){
-
-    let _el = $(`<confirmar class='modal fade' tabindex='-1' aria-hidden='true'>
-                    <div class='modal-dialog modal-dialog-centered' role='document'>
-                        <div class='modal-content rounded-3 shadow'>
-                            <div class='modal-body p-4 text-center'>
-                                <h5 class='mb-0 title'>${title}</h5>
-                                <p class='mb-0 desc'>${description}</p>
-                            </div>
-                            <div class='modal-footer flex-nowrap p-0'>
-                                <button type='button' class='btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 cancel'>
-                                    Cancelar
-                                </button>
-                                <button type='button' class='btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-start confirm'>
-                                    <strong>Confirmar</strong>
-                                    </button>
-                            </div>
-                        </div>
-                    </div>
-                </confirmar>`);
+    let _el = $(`<confirmar class='modal fade' tabindex='-1' aria-hidden='true'><div class='modal-dialog modal-dialog-centered' role='document'><div class='modal-content rounded-3 shadow'><div class='modal-body p-4 text-center'><h5 class='mb-0 title'>${title}</h5><p class='mb-0 desc'>${description}</p></div><div class='modal-footer flex-nowrap p-0'><button type='button' class='btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 cancel'>Cancelar</button><button type='button' class='btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-start confirm'><strong>Confirmar</strong></button></div></div></div></confirmar>`);
 
     return new Promise((resolve, reject) => {
         $("body").append(_el);
@@ -29,7 +8,7 @@ function confirmar(title, description){
         _el.modal("toggle");
 
 
-        $('#confirmModal .confirm').one('click',"button", function (e) {
+        $('#confirmModal .confirm').one('click', "button", function (e) {
             if ($(e.currentTarget).hasClass(".confirm")) {
                 resolve(true);
             }
