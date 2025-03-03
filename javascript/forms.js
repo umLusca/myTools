857053628e4d1737a564905871d89685e98440d7
Lucas Pinheiro <lucaspinheiro@duck.com>
2025-03-03 17:03:16 -0400

    Fix Action

--- javascript/forms.js
+++ javascript/forms.js
@@ -55,13 +55,13 @@ $("form[ajax]").on("submit", (e) => {
 		let data = $($this).getFormObject();
 		
 		//por padrão é o do formulário.
-		data["query"] = $($this).attr("action");
+		let action = $($this).attr("action");
 		
 		if (e.originalEvent) {
 			//Ele poderá ser substituido pelo o do botão, caso exista.
 			let buttonAction = $(e.originalEvent.submitter).attr("formaction");
 			if (typeof buttonAction === "string" && buttonAction.length >= 2) {
-				data["query"] = buttonAction;
+				action = buttonAction;
 			}
 		}
 		$($this).find(".return").addClass("alert-info").removeClass("alert-danger").text("Aguarde...").slideDown();
